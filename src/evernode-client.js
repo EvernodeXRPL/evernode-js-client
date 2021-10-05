1c4e860754d6b49fb3c599936ad90567ac8ffbe3
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-10-05 23:40:23 +0530

    Added parallel transaction submission support.

--- src/evernode-client.js
+++ src/evernode-client.js
@@ -51,10 +51,6 @@ class EvernodeClient {
         this.xrplAcc = new XrplAccount(this.rippleAPI, this.xrpAddress, this.xrpSecret);
         this.accKeyPair = this.xrplAcc.deriveKeypair();
         this.evernodeHookAcc = new XrplAccount(this.rippleAPI, this.hookAddress);
-        this.ledgerSequence = this.rippleAPI.getLedgerVersion();
-        this.rippleAPI.events.on(RippleAPIEvents.LEDGER, async (e) => {
-            this.ledgerSequence = e.ledgerVersion;
-        });
     }
 
     async redeem(hostingToken, hostAddress, amount, requirement) {
@@ -71,7 +67,7 @@ class EvernodeClient {
                     console.log(`Waiting for redeem response...`);
                     // Handle the transactions on evernode account and filter out redeem operations.
                     const failTimeout = setInterval(() => {
-                        if (this.ledgerSequence - res.ledgerVersion >= REDEEM_TIMEOUT_WINDOW) {
+                        if (this.rippleAPI.ledgerVersion - res.ledgerVersion >= REDEEM_TIMEOUT_WINDOW) {
                             clearInterval(failTimeout);
                             reject({ error: ErrorCodes.REDEEM_ERR, reason: `No response within ${REDEEM_TIMEOUT_WINDOW} ledgers time.`, redeemTxHash: res.txHash });
                         }
@@ -133,7 +129,7 @@ class EvernodeClient {
                     null,
                     [{ type: MemoTypes.AUDIT_REQ, format: MemoFormats.BINARY, data: '' }]);
                 if (res) {
-                    const startingLedger = this.ledgerSequence;
+                    const startingLedger = this.rippleAPI.ledgerVersion;
                     console.log('Waiting for check...');
                     const getChecksAndProcess = () => {
                         return new Promise(async (resolve, reject) => {
@@ -162,7 +158,7 @@ class EvernodeClient {
                                         reject({ error: ErrorCodes.AUDIT_REQ_ERROR, reason: 'Cashing check failed.' });
                                 } else if (resp && resp.account_objects.length === 0) {
                                     const timeout = setTimeout(() => {
-                                        if (this.ledgerSequence - startingLedger >= MOMENT_SIZE) {
+                                        if (this.rippleAPI.ledgerVersion - startingLedger >= MOMENT_SIZE) {
                                             clearTimeout(timeout);
                                             reject({ error: ErrorCodes.AUDIT_REQ_ERROR, reason: `No checks found within moment(${MOMENT_SIZE}) window.` });
                                         }
