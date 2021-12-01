cd01c62a2b0d57ad8dfc401ae450626daf26dcdf
ravinsp <33562092+ravinsp@users.noreply.github.com>
2021-12-01 18:19:40 +0530

    Fixed xfl issue.

--- src/clients/base-evernode-client.js
+++ src/clients/base-evernode-client.js
@@ -140,7 +140,7 @@ class BaseEvernodeClient {
                         user: codec.encodeAccountID(buf.slice(0, 20)),
                         host: tx.Destination,
                         token: buf.slice(28, 31).toString(),
-                        moments: parseInt(XflHelpers.toString(buf.slice(20, 28))),
+                        moments: parseInt(XflHelpers.toString(buf.slice(20, 28).readBigInt64BE(0))),
                         payload: payload
                     }
                 }
