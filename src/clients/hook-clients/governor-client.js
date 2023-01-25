08e81f412fa18b1dfb45965e0457467f6f453f81
Udith-Gayan <udith.indrakantha@gmail.com>
2023-01-25 18:31:08 +0530

    r

--- src/clients/hook-clients/governor-client.js
+++ src/clients/hook-clients/governor-client.js
@@ -1,10 +1,11 @@
+const { BaseEvernodeClient } = require("../../../dist");
 const { DefaultValues } = require("../../defaults");
 
 const GovernorEvents = {
     RegistryInitialized: EvernodeEvents.RegistryInitialized
 }
 
-class GovernorClient {
+class GovernorClient extends BaseEvernodeClient {
     constructor(options = {}) {
         super((options.governorAddress || DefaultValues.governorAddress), null, Object.values(GovernorEvents), false, options);
     }
