76e2b10e10f62051c616f829f74e236c72dc6cb2
kithminisg <kithminishanika@gmail.com>
2023-03-22 17:52:22 +0530

    Missed uniqueId addition

--- src/clients/base-evernode-client.js
+++ src/clients/base-evernode-client.js
@@ -804,7 +804,7 @@ class BaseEvernodeClient {
                     return { ...ownerStateDecoded, ...idStateDecoded };
                 }
 
-                return idStateDecoded;
+                return { ...idStateDecoded, uniqueId: candidateId };
             }
         }
         catch (e) {
