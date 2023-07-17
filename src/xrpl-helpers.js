072cc4bd0013465353704f3f267c83da8b34fc4e
kasunkavinda <kasunkavinda.bc@gmail.com>
2023-07-17 09:55:24 +0530

    Definition fix

--- src/xrpl-helpers.js
+++ src/xrpl-helpers.js
@@ -1,19 +1,19 @@
 const { sign, derive, XrplDefinitions, binary } = require('xrpl-accountlib')
-class XrplHelpers{
+class XrplHelpers {
     definitions;
     constructor(definition) {
         this.definitions = new XrplDefinitions(definition);
     }
-    encode(transacion){
-        return binary.encode(transacion);
+    encode(transacion) {
+        return binary.encode(transacion, this.definitions);
     }
-    decode(transacion){
-        return binary.decode(transacion)
+    decode(transacion) {
+        return binary.decode(transacion, this.definitions)
     }
 
-    sign(tx, secret,isMultiSign = false) {
+    sign(tx, secret, isMultiSign = false) {
         const account = derive.familySeed(secret);
-        const signed = sign(tx, (isMultiSign ? [account.signAs(account.address)] : account));
+        const signed = sign(tx, (isMultiSign ? [account.signAs(account.address)] : account), this.definitions);
         return {
             hash: signed.id,
             tx_blob: signed.signedTransaction
