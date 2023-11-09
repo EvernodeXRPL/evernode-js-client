610ee23bbe5110924fa1cf9e000e831229b14806
chalith <desaman.chalith@gmail.com>
2023-11-09 13:43:52 +0530

    Modified post install

--- clean-pkg.sh
+++ clean-pkg.sh
@@ -3,5 +3,5 @@
 rm -rf dist/resources
 
 # Create a clean dist/package.json for publishing.
-node -p "const pkg=require('./package.json'); delete pkg.scripts; delete pkg.devDependencies; JSON.stringify(pkg, null, 4)" \
+node -p "const pkg=require('./package.json'); delete pkg.scripts; delete pkg.devDependencies; pkg.scripts = { postinstall : 'node postinstall.js external'};JSON.stringify(pkg, null, 4)" \
     >dist/package.json
