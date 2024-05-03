a2d997c9a6a15cfb39302e7d74db3929452c6fc0
Kithmini Gunawardhana <kithminishanika@gmail.com>
2024-05-03 14:58:37 +0530

    Modified the Governance Game New Hook Candidiate propsong mechanism (#212)

--- src/clients/base-evernode-client.js
+++ src/clients/base-evernode-client.js
@@ -14,10 +14,12 @@ const { HookHelpers } = require('../hook-helpers');
 const xrpl = require('xrpl');
 
 const CANDIDATE_PROPOSE_HASHES_PARAM_OFFSET = 0;
-const CANDIDATE_PROPOSE_KEYLETS_PARAM_OFFSET = 96;
-const CANDIDATE_PROPOSE_UNIQUE_ID_PARAM_OFFSET = 198;
-const CANDIDATE_PROPOSE_SHORT_NAME_PARAM_OFFSET = 230;
-const CANDIDATE_PROPOSE_PARAM_SIZE = 250;
+const CANDIDATE_PROPOSE_KEYLETS_PARAM_OFFSET = 128;
+const CANDIDATE_PROPOSE_UNIQUE_ID_PARAM_OFFSET = 264;
+const CANDIDATE_PROPOSE_SHORT_NAME_PARAM_OFFSET = 296;
+const CANDIDATE_PROPOSE_PARAM_SIZE = 316;
+
+const MAX_HOOK_PARAM_SIZE = 256;
 
 const DUD_HOST_CANDID_ADDRESS_OFFSET = 12;
 
@@ -978,8 +980,8 @@ class BaseEvernodeClient {
      */
     async _propose(hashes, shortName, options = {}) {
         const hashesBuf = Buffer.from(hashes, 'hex');
-        if (!hashesBuf || hashesBuf.length != 96)
-            throw 'Invalid hashes: Hashes should contain all three Governor, Registry, Heartbeat hook hashes.';
+        if (!hashesBuf || hashesBuf.length != 128)
+            throw 'Invalid hashes: Hashes should contain all three Governor, Registry, Heartbeat, Reputation hook hashes.';
 
         // Check whether hook hashes exist in the definition.
         let keylets = [];
@@ -1010,7 +1012,9 @@ class BaseEvernodeClient {
             {
                 hookParams: [
                     { name: HookParamKeys.PARAM_EVENT_TYPE_KEY, value: EventTypes.CANDIDATE_PROPOSE },
-                    { name: HookParamKeys.PARAM_EVENT_DATA_KEY, value: paramBuf.toString('hex').toUpperCase() }
+                    { name: HookParamKeys.PARAM_EVENT_DATA_KEY, value: paramBuf.slice(0, MAX_HOOK_PARAM_SIZE).toString('hex').toUpperCase() },
+                    { name: HookParamKeys.PARAM_EVENT_DATA2_KEY, value: paramBuf.slice(MAX_HOOK_PARAM_SIZE).toString('hex').toUpperCase() }
+
                 ],
                 ...options.transactionOptions
             });
