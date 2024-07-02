18c0531162a1317e4e47630e39c982a4a3a69199
chalith <desaman.chalith@gmail.com>
2024-07-02 10:50:24 +0530

    Comment change

--- src/clients/host-client.js
+++ src/clients/host-client.js
@@ -573,13 +573,14 @@ class HostClient extends BaseEvernodeClient {
         }
 
         let uriToken = await this.xrplAcc.getURITokenByUri(uri);
-        // If uri token is not found in first tre, Retry again.
+        // If uri token is not found in first try, Retry again.
         if (!uriToken) {
             console.log(`URI token not found, Retrying in 1 second.`)
             await new Promise(resolve => setTimeout(resolve, 1000));
             uriToken =  await this.xrplAcc.getURITokenByUri(uri);
         }
         
+        // Throw if still not found.
         if (!uriToken)
             throw "Offer lease URI token not found.";
 
